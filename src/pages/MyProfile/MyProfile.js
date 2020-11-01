7d80f1bdd5140bda1d460defe1517d08491ba92d
unknown <anzamarska@gmail.com>
2020-11-01 16:04:28 +0100

    install react-loader-spinner, add loader on fetch call

--- src/pages/MyProfile/MyProfile.js
+++ src/pages/MyProfile/MyProfile.js
@@ -1,27 +1,39 @@
 import React, { useEffect, useState } from 'react';
-import { WrapperUserInfo, AvatarUser } from './styles';
+import { WrapperUserInfo, AvatarUser,  } from './styles';
+import Loader from 'react-loader-spinner';
+
         
 function MyProfile() {
 
-    const [user, setUser] = useState({userName:"", avatar_url: ""})
+    const [user, setUser] = useState({userName:"", avatar_url: "", fetchInProgress: true})
 
     useEffect(() => {
         fetch('https://api.github.com/users/anzamarska')
         .then( response => response.json())
         .then((data) => {
             setUser({
-                userName: data.bio,
+                userName: data.name,
                 avatar_url: data.avatar_url,
-            })
+                fetchInProgress: false,
+            })   
         })
         .catch( error => console.error(error));
     }, [] );
 
     return (
-        <WrapperUserInfo>
-            <h1>{user.userName}</h1>
-            <AvatarUser src={user.avatar_url} alt="user profile"/>  
-        </WrapperUserInfo>
+        <>
+            {(user.fetchInProgress)
+               ? <Loader
+                type="Oval"
+                color="#0000FF"
+                height={80}
+                width={80}
+                />
+            :<WrapperUserInfo>
+                <h1>{user.userName}</h1>
+                <AvatarUser src={user.avatar_url} alt="user profile"/>  
+            </WrapperUserInfo>}
+        </>
     )
 }
 
